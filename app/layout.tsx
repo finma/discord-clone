854e71bde88d1ea622a4648aa896d611cb22e483
finma <firmanmaulana2403@gmail.com>
2024-07-13 22:41:01 +0700

    add: create server modal

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,10 +2,11 @@ import type { Metadata } from "next";
 import { Open_Sans } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 
+import { cn } from "@/lib/utils";
 import { ThemeProvider } from "@/components/providers/theme-provider";
+import { ModalProvider } from "@/components/providers/modal-provider";
 
 import "./globals.css";
-import { cn } from "@/lib/utils";
 
 const font = Open_Sans({ subsets: ["latin"] });
 
@@ -29,6 +30,7 @@ export default function RootLayout({
             enableSystem={false}
             storageKey="connectify-theme"
           >
+            <ModalProvider />
             {children}
           </ThemeProvider>
         </body>
--- components/navigation/navigation-action.tsx
+++ components/navigation/navigation-action.tsx
@@ -3,12 +3,18 @@
 import { Plus } from "lucide-react";
 
 import { ActionTooltip } from "@/components/action-tooltip";
+import { useModal } from "@/hooks/use-modal-store";
 
 export const NavigationAction = () => {
+  const { onOpen } = useModal();
+
   return (
     <div>
       <ActionTooltip label="Create a server" side="right" align="center">
-        <button className="group flex items-center">
+        <button
+          onClick={() => onOpen("create-server")}
+          className="group flex items-center"
+        >
           <div className="size-12 flex items-center justify-center rounded-[24px] bg-background overflow-hidden dark:bg-neutral-700 transition-all group-hover:rounded-[12px] group-hover:bg-emerald-500 ">
             <Plus
               size={25}
