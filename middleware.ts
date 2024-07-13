9f71414bb86de5289d5a98d8d9dea9f49239f0d0
finma <firmanmaulana2403@gmail.com>
2024-07-13 16:30:22 +0700

    add: uploadthing for upload image & file

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,13 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher(["/(.*)"]);
+const isPublicRoute = createRouteMatcher([
+  "/sign-in(.*)",
+  "/sign-up(.*)",
+  "/api/uploadthing(.*)",
+]);
 
-export default clerkMiddleware((auth, req) => {
-  if (
-    isProtectedRoute(req) &&
-    req.nextUrl.pathname !== "/sign-in" &&
-    req.nextUrl.pathname !== "/sign-up"
-  ) {
+export default clerkMiddleware((auth, request) => {
+  if (!isPublicRoute(request)) {
     auth().protect();
   }
 });
