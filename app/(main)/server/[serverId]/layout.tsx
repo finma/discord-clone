10591e22834af5398dc4db3ab3e1e91751d0accf
finma <firmanmaulana2403@gmail.com>
2024-07-15 09:33:45 +0700

    add: server layout & server sidebard component

---
+++ app/(main)/server/[serverId]/layout.tsx
@@ -0,0 +1,48 @@
+import { redirect } from "next/navigation";
+import { auth } from "@clerk/nextjs/server";
+
+import { db } from "@/lib/db";
+import { currentProfile } from "@/lib/current-profile";
+import { ServerSidebar } from "@/components/server/server-sidebar";
+
+export default async function ServerIdLayout({
+  children,
+  params,
+}: {
+  children: React.ReactNode;
+  params: {
+    serverId: string;
+  };
+}) {
+  const profile = await currentProfile();
+
+  if (!profile) {
+    return auth().redirectToSignIn();
+  }
+
+  const server = await db.server.findUnique({
+    where: {
+      id: params.serverId,
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+  });
+
+  if (!server) {
+    return redirect("/");
+  }
+
+  return (
+    <div className="h-full">
+      {/* Sidebar */}
+      <div className="fixed hidden md:flex h-full w-60 z-20 flex-col inset-y-0">
+        <ServerSidebar serverId={params.serverId} />
+      </div>
+
+      <main className="h-full pl-60">{children}</main>
+    </div>
+  );
+}
---
+++ components/server/server-header.tsx
@@ -0,0 +1,81 @@
+"use client";
+
+import { MemberRole } from "@prisma/client";
+
+import { ServerWithMembersWithProfiles } from "@/types";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import {
+  ChevronDown,
+  LogOut,
+  PlusCircle,
+  Settings,
+  Trash,
+  UserPlus,
+  Users,
+} from "lucide-react";
+
+interface ServerHeaderProps {
+  server: ServerWithMembersWithProfiles;
+  role?: MemberRole;
+}
+
+export const ServerHeader = ({ server, role }: ServerHeaderProps) => {
+  const isAdmin = role === MemberRole.ADMIN;
+  const isModerator = isAdmin || role === MemberRole.MODERATOR;
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger className="focus:outline-none" asChild>
+        <button className="w-full h-12 text-base font-semibold px-3 flex items-center border-neutral-200 dark:border-neutral-800 border-b-2 hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition">
+          {server.name}
+          <ChevronDown className="ml-auto size-5" />
+        </button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-56 text-sm font-medium text-black dark:text-neutral-400 space-y-[2px]">
+        {isModerator && (
+          <DropdownMenuItem className="text-indigo-600 dark:text-indigo-400 px-3 py-2 text-sm cursor-pointer">
+            Invite People
+            <UserPlus className="ml-auto size-4" />
+          </DropdownMenuItem>
+        )}
+        {isAdmin && (
+          <DropdownMenuItem className="px-3 py-2 text-sm cursor-pointer">
+            Server Settings
+            <Settings className="ml-auto size-4" />
+          </DropdownMenuItem>
+        )}
+        {isAdmin && (
+          <DropdownMenuItem className="px-3 py-2 text-sm cursor-pointer">
+            Manage Members
+            <Users className="ml-auto size-4" />
+          </DropdownMenuItem>
+        )}
+        {isModerator && (
+          <DropdownMenuItem className="px-3 py-2 text-sm cursor-pointer">
+            Create Channel
+            <PlusCircle className="ml-auto size-4" />
+          </DropdownMenuItem>
+        )}
+        {isModerator && <DropdownMenuSeparator />}
+        {isAdmin && (
+          <DropdownMenuItem className="text-rose-500 px-3 py-2 text-sm cursor-pointer">
+            Delete Server
+            <Trash className="ml-auto size-4" />
+          </DropdownMenuItem>
+        )}
+        {!isAdmin && (
+          <DropdownMenuItem className="px-3 py-2 text-sm cursor-pointer">
+            Leave Server
+            <LogOut className="ml-auto size-4" />
+          </DropdownMenuItem>
+        )}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
