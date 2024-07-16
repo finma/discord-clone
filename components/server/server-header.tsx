32059374a6b1f9a044b9f0d4881a42af9d8cdc81
finma <firmanmaulana2403@gmail.com>
2024-07-16 21:36:43 +0700

    add: leave server component

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -85,7 +85,10 @@ export const ServerHeader = ({ server, role }: ServerHeaderProps) => {
           </DropdownMenuItem>
         )}
         {!isAdmin && (
-          <DropdownMenuItem className="text-rose-500 px-3 py-2 text-sm cursor-pointer">
+          <DropdownMenuItem
+            onClick={() => onOpen("leave-server", { server })}
+            className="text-rose-500 px-3 py-2 text-sm cursor-pointer"
+          >
             Leave Server
             <LogOut className="ml-auto size-4" />
           </DropdownMenuItem>
