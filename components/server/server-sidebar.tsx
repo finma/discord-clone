cd2e0f09b2dd031a03b1fd4207d9147afaebb537
finma <firmanmaulana2403@gmail.com>
2024-07-18 08:25:35 +0700

    fix: when data length 0 will be false

--- components/server/server-sidebar.tsx
+++ components/server/server-sidebar.tsx
@@ -129,7 +129,7 @@ export const ServerSidebar = async ({ serverId }: ServerSidebarProps) => {
           />
         </div>
         <Separator className="bg-zinc-200 dark:bg-zinc-700 rounded-md my-2" />
-        {textChannels?.length && (
+        {!!textChannels?.length && (
           <div className="mb-2">
             <ServerSection
               label="Text Channels"
@@ -149,7 +149,7 @@ export const ServerSidebar = async ({ serverId }: ServerSidebarProps) => {
             </div>
           </div>
         )}
-        {audioChannels?.length && (
+        {!!audioChannels?.length && (
           <div className="mb-2">
             <ServerSection
               label="Voice Channels"
@@ -169,7 +169,7 @@ export const ServerSidebar = async ({ serverId }: ServerSidebarProps) => {
             </div>
           </div>
         )}
-        {videoChannels?.length && (
+        {!!videoChannels?.length && (
           <div className="mb-2">
             <ServerSection
               label="Video Channels"
