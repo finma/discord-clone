021b2607b0f58754e10ba5588db504d33851f6f8
finma <firmanmaulana2403@gmail.com>
2024-07-18 10:33:53 +0700

    update: add initial channel and redirect to initial channel

--- app/(main)/server/[serverId]/page.tsx
+++ app/(main)/server/[serverId]/page.tsx
@@ -1,3 +1,48 @@
-export default function ServerIdPage() {
-  return <div>ServerIdPage</div>;
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+
+import { db } from "@/lib/db";
+import { currentProfile } from "@/lib/current-profile";
+
+interface ServerSidebarProps {
+  params: {
+    serverId: string;
+  };
+}
+
+export default async function ServerIdPage({ params }: ServerSidebarProps) {
+  const profile = await currentProfile();
+
+  if (!profile) {
+    return auth().redirectToSignIn();
+  }
+
+  const server = await db.server.findUnique({
+    where: {
+      id: params.serverId,
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+    include: {
+      channels: {
+        where: {
+          name: "general",
+        },
+        orderBy: {
+          createdAt: "asc",
+        },
+      },
+    },
+  });
+
+  const initialChannel = server?.channels[0];
+
+  if (initialChannel?.name !== "general") {
+    return null;
+  }
+
+  return redirect(`/server/${server?.id}/channels/${initialChannel?.id}`);
 }
