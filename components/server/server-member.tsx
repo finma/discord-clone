8bfb246b70dc7229e0bd80e0b0f54843ba4693d3
finma <firmanmaulana2403@gmail.com>
2024-07-18 10:34:19 +0700

    add: route to member page

--- components/server/server-member.tsx
+++ components/server/server-member.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useParams } from "next/navigation";
+import { useParams, useRouter } from "next/navigation";
 import { ShieldAlert, ShieldCheck } from "lucide-react";
 import { Member, MemberRole, Profile, Server } from "@prisma/client";
 
@@ -22,11 +22,17 @@ const roleIconMap = {
 
 export const ServerMember = ({ server, member }: ServerMemberProps) => {
   const params = useParams();
+  const router = useRouter();
 
   const icon = roleIconMap[member.role];
 
+  const onClick = () => {
+    router.push(`/server/${server.id}/conversations/${member.id}`);
+  };
+
   return (
     <button
+      onClick={onClick}
       className={cn(
         "group p-2 rounded-md flex items-center gap-x-2 w-full hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition mb-1",
         params?.memberId === member.id && "bg-zinc-700/20 dark:bg-zinc-700"
