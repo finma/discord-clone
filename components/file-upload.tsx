d628a0a96a0c6ff4f1a441c2c5805d0f10a69c34
finma <firmanmaulana2403@gmail.com>
2024-07-21 21:44:39 +0700

    update: upload pdf

--- components/file-upload.tsx
+++ components/file-upload.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { X } from "lucide-react";
+import { FileIcon, X } from "lucide-react";
 import Image from "next/image";
 
 import { UploadDropzone } from "@/lib/uploadthing";
@@ -28,6 +28,29 @@ export const FileUpload = ({ endpoint, value, onChange }: FileUploadProps) => {
           type="button"
           onClick={() => onChange("")}
           className="bg-rose-500 text-white rounded-full shadow-sm absolute top-0 right-0 p-1"
+        >
+          <X className="size-4" />
+        </button>
+      </div>
+    );
+  }
+
+  if (value && fileType === "pdf") {
+    return (
+      <div className="relative flex items-center p-2 mt-2 rounded-md bg-indigo-100 dark:bg-indigo-500/30">
+        <FileIcon className="size-10 fill-indigo-200 stroke-indigo-400" />
+        <a
+          href={value}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="ml-2 text-sm text-indigo-500 dark:text-indigo-400"
+        >
+          {value}
+        </a>
+        <button
+          type="button"
+          onClick={() => onChange("")}
+          className="bg-rose-500 text-white rounded-full shadow-sm absolute -top-2 -right-2 p-1"
         >
           <X className="size-4" />
         </button>
