8b653c9a02949ac85a3eb3c2001d600565dab135
finma <firmanmaulana2403@gmail.com>
2024-07-21 21:13:26 +0700

    add: chat input component

--- app/(main)/server/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/server/[serverId]/channels/[channelId]/page.tsx
@@ -4,6 +4,7 @@ import { redirect } from "next/navigation";
 import { db } from "@/lib/db";
 import { currentProfile } from "@/lib/current-profile";
 import { ChatHeader } from "@/components/chat/chat-header";
+import { ChatInput } from "@/components/chat/chat-input";
 
 interface ChannelIdPageProps {
   params: {
@@ -42,6 +43,16 @@ export default async function ChannelIdPage({ params }: ChannelIdPageProps) {
         serverId={channel.serverId}
         name={channel.name}
         type="channel"
+      />
+      <div className="flex-1">Future Message</div>
+      <ChatInput
+        apiUrl="/api/socket/messages"
+        name={channel.name}
+        type="channel"
+        query={{
+          channelId: channel.id,
+          serverId: channel.serverId,
+        }}
       />
     </div>
   );
