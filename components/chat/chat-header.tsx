a8539bb1bdac93f541bea56f94da4e4e38650262
finma <firmanmaulana2403@gmail.com>
2024-07-21 20:15:44 +0700

    add: socket indicator component

--- components/chat/chat-header.tsx
+++ components/chat/chat-header.tsx
@@ -2,6 +2,7 @@ import { Hash } from "lucide-react";
 
 import { UserAvatar } from "@/components/user-avatar";
 import { MobileToggle } from "@/components/mobile-toggle";
+import { SocketIndicator } from "@/components/socket-indicator";
 
 interface ChatHeaderProps {
   serverId: string;
@@ -28,6 +29,9 @@ export const ChatHeader = ({
       <p className="text-base font-semibold text-black dark:text-white">
         {name}
       </p>
+      <div className="ml-auto flex items-center">
+        <SocketIndicator />
+      </div>
     </div>
   );
 };
