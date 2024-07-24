c7d21024041eb5b220f70ced619f114a90fecb86
finma <firmanmaulana2403@gmail.com>
2024-07-24 15:06:07 +0700

    add: chat item component with handle edit

---
+++ components/chat/chat-item.tsx
@@ -0,0 +1,243 @@
+"use client";
+
+import * as z from "zod";
+import axios from "axios";
+import qs from "query-string";
+import Image from "next/image";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useEffect, useState } from "react";
+import { Member, MemberRole, Profile } from "@prisma/client";
+import { Edit, FileIcon, ShieldAlert, ShieldCheck, Trash } from "lucide-react";
+
+import { cn } from "@/lib/utils";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { UserAvatar } from "@/components/user-avatar";
+import { ActionTooltip } from "@/components/action-tooltip";
+import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
+
+interface ChatItemProps {
+  id: string;
+  content: string;
+  member: Member & { profile: Profile };
+  timestamp: string;
+  fileUrl: string | null;
+  deleted: boolean;
+  currentMember: Member;
+  isUpdated: boolean;
+  socketUrl: string;
+  socketQuery: Record<string, string>;
+}
+
+const roleIconMap = {
+  GUEST: null,
+  ADMIN: <ShieldAlert className="size-4 text-rose-500 ml-2" />,
+  MODERATOR: <ShieldCheck className="size-4 text-indigo-500 ml-2" />,
+};
+
+const formSchema = z.object({
+  content: z.string().min(1),
+});
+
+export const ChatItem = ({
+  id,
+  content,
+  member,
+  timestamp,
+  fileUrl,
+  deleted,
+  currentMember,
+  isUpdated,
+  socketUrl,
+  socketQuery,
+}: ChatItemProps) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [isDeleting, setIsDeleting] = useState(false);
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      content: content,
+    },
+  });
+
+  const isLoading = form.formState.isSubmitting;
+
+  useEffect(() => {
+    form.reset({
+      content: content,
+    });
+  }, [content, form]);
+
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      if (event.key === "Escape") {
+        setIsEditing(false);
+      }
+    };
+
+    document.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, []);
+
+  const fileType = fileUrl?.split(".").pop();
+
+  const isAdmin = currentMember.role === MemberRole.ADMIN;
+  const isModerator = currentMember.role === MemberRole.MODERATOR;
+  const isOwner = currentMember.id === member.id;
+  const canDeleteMessage = !deleted && (isAdmin || isModerator || isOwner);
+  const canEditMessage = !deleted && isOwner && !fileUrl;
+  const isPDF = fileType === "pdf" && fileUrl;
+  const isImage = !isPDF && fileUrl;
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      const url = qs.stringifyUrl({
+        url: `${socketUrl}/${id}`,
+        query: socketQuery,
+      });
+
+      await axios.patch(url, values);
+
+      form.reset();
+      setIsEditing(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div className="relative group flex items-center hover:bg-black/5 p-4 transition w-full">
+      <div className="group flex items-start w-full gap-x-2">
+        {/* Avatar */}
+        <div className="cursor-pointer hover:drop-shadow-md transition">
+          <UserAvatar src={member.profile.imageUrl} />
+        </div>
+
+        <div className="flex flex-col w-full">
+          {/* Header */}
+          <div className="flex items-center gap-x-2">
+            <div className="flex items-center">
+              <p className="text-sm font-semibold hover:underline cursor-pointer">
+                {member.profile.name}
+              </p>
+              <ActionTooltip label={member.role}>
+                {roleIconMap[member.role]}
+              </ActionTooltip>
+            </div>
+            <span className="text-xs text-zinc-500 dark:text-zinc-400">
+              {timestamp}
+            </span>
+          </div>
+
+          {/* Showing Image */}
+          {isImage && (
+            <a
+              href={fileUrl}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="relative aspect-square rounded-md mt-2 overflow-hidden border flex items-center bg-secondary size-48"
+            >
+              <Image
+                src={fileUrl}
+                alt={content}
+                fill
+                className="object-cover"
+              />
+            </a>
+          )}
+
+          {/* Showing PDF */}
+          {isPDF && (
+            <div className="relative flex items-center p-2 mt-2 rounded-md bg-indigo-100 dark:bg-indigo-500/15">
+              <FileIcon className="size-10 fill-indigo-200 stroke-indigo-400" />
+              <a
+                href={content}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="ml-2 text-sm text-indigo-500 dark:text-indigo-400"
+              >
+                File PDF
+              </a>
+            </div>
+          )}
+
+          {/* Showing content text */}
+          {!fileUrl && !isEditing && (
+            <p
+              className={cn(
+                "text-sm text-zinc-600 dark:text-zinc-300",
+                deleted &&
+                  "text-xs text-zinc-500 dark:text-zinc-400 italic mt-1"
+              )}
+            >
+              {content}
+              {isUpdated && !deleted && (
+                <span className="text-[10px] mx-2 text-zinc-500 dark:text-zinc-400 ">
+                  (edited)
+                </span>
+              )}
+            </p>
+          )}
+
+          {/* Form Editing Content */}
+          {!fileUrl && isEditing && (
+            <Form {...form}>
+              <form
+                onSubmit={form.handleSubmit(onSubmit)}
+                className="flex items-center gap-x-2 w-full pt-2"
+              >
+                <FormField
+                  name="content"
+                  control={form.control}
+                  render={({ field }) => (
+                    <FormItem className="flex-1">
+                      <FormControl>
+                        <div className="relative w-full">
+                          <Input
+                            disabled={isLoading}
+                            placeholder="Edited message"
+                            autoComplete="off"
+                            className="p-2 bg-zinc-200/90 dark:bg-zinc-700/75 border-0 border-none focus-visible:ring-0 focus-visible:ring-offset-0 text-zinc-600 dark:text-zinc-200"
+                            {...field}
+                          />
+                        </div>
+                      </FormControl>
+                    </FormItem>
+                  )}
+                />
+                <Button variant="primary" size="sm" disabled={isLoading}>
+                  Save
+                </Button>
+              </form>
+              <span className="text-[10px] mt-1 text-zinc-400 ">
+                Press escape to cancel, enter to save
+              </span>
+            </Form>
+          )}
+        </div>
+      </div>
+
+      {/* Editing and deleting buttons */}
+      {canDeleteMessage && (
+        <div className="hidden group-hover:flex items-center gap-x-2 absolute p-1 -top-2 right-5 bg-white dark:bg-zinc-800 border rounded-sm">
+          {canEditMessage && (
+            <ActionTooltip label="Edit">
+              <Edit
+                onClick={() => setIsEditing(true)}
+                className="cursor-pointer ml-auto size-4 text-zinc-500 dark:text-zinc-600 dark:hover:text-zinc-300 transition"
+              />
+            </ActionTooltip>
+          )}
+          <ActionTooltip label="Delete">
+            <Trash className="cursor-pointer ml-auto size-4 text-zinc-500 dark:text-zinc-600 dark:hover:text-zinc-300 transition" />
+          </ActionTooltip>
+        </div>
+      )}
+    </div>
+  );
+};
