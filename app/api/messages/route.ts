ad105d5de47198bcd1760da0f0a3cdbc7a07b04e
finma <firmanmaulana2403@gmail.com>
2024-07-24 10:18:44 +0700

    add: api to get messages

---
+++ app/api/messages/route.ts
@@ -0,0 +1,81 @@
+import { NextResponse } from "next/server";
+import { Message } from "@prisma/client";
+
+import { db } from "@/lib/db";
+import { currentProfile } from "@/lib/current-profile";
+
+const MESSAGES_BATCH_SIZE = 50;
+
+export async function GET(req: Request) {
+  try {
+    const profile = await currentProfile();
+    const { searchParams } = new URL(req.url);
+
+    const cursor = searchParams.get("cursor");
+    const channelId = searchParams.get("channelId");
+
+    if (!profile) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    if (!channelId) {
+      return new NextResponse("Channel Id Missing", { status: 400 });
+    }
+
+    let messages: Message[] = [];
+
+    if (cursor) {
+      messages = await db.message.findMany({
+        take: MESSAGES_BATCH_SIZE,
+        skip: 1,
+        cursor: {
+          id: cursor,
+        },
+        where: {
+          channelId,
+        },
+        include: {
+          member: {
+            include: {
+              profile: true,
+            },
+          },
+        },
+        orderBy: {
+          createdAt: "desc",
+        },
+      });
+    } else {
+      messages = await db.message.findMany({
+        take: MESSAGES_BATCH_SIZE,
+        where: {
+          channelId,
+        },
+        include: {
+          member: {
+            include: {
+              profile: true,
+            },
+          },
+        },
+        orderBy: {
+          createdAt: "desc",
+        },
+      });
+    }
+
+    let nextCursor = null;
+
+    if (messages.length === MESSAGES_BATCH_SIZE) {
+      nextCursor = messages[messages.length - 1].id;
+    }
+
+    return NextResponse.json({
+      items: messages,
+      nextCursor,
+    });
+  } catch (error) {
+    console.log("[MESSAGES_GET]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
