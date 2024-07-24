81248fcd948925e6b78931fa12039ec93ce34fc6
finma <firmanmaulana2403@gmail.com>
2024-07-24 16:49:48 +0700

    update: add button to load previous message

--- components/chat/chat-messages.tsx
+++ components/chat/chat-messages.tsx
@@ -1,9 +1,9 @@
 "use client";
 
-import { Fragment } from "react";
 import { format } from "date-fns";
 import { Member } from "@prisma/client";
 import { Loader2, ServerCrash } from "lucide-react";
+import { ElementRef, Fragment, useRef } from "react";
 
 import { useChatQuery } from "@/hooks/use-chat-query";
 import { MessageWithMemberWithProfile } from "@/types";
@@ -41,6 +41,9 @@ export const ChatMessages = ({
   const addKey = `chat:${chatId}:message`;
   const updateKey = `chat:${chatId}:message:update`;
 
+  const chatRef = useRef<ElementRef<"div">>(null);
+  const bottomRef = useRef<ElementRef<"div">>(null);
+
   const { data, fetchNextPage, hasNextPage, isFetchingNextPage, status } =
     useChatQuery({
       queryKey,
@@ -74,9 +77,24 @@ export const ChatMessages = ({
   }
 
   return (
-    <div className="flex flex-1 flex-col py-4 overflow-y-auto">
-      <div className="flex-1" />
-      <ChatWelcome type={type} name={name} />
+    <div ref={chatRef} className="flex flex-1 flex-col py-4 overflow-y-auto">
+      {!hasNextPage && <div className="flex-1" />}
+      {!hasNextPage && <ChatWelcome type={type} name={name} />}
+
+      {hasNextPage && (
+        <div className="flex justify-center">
+          {isFetchingNextPage ? (
+            <Loader2 className="size-6 text-zinc-500 animate-spin my-4" />
+          ) : (
+            <button
+              onClick={() => fetchNextPage()}
+              className="text-xs my-4 text-zinc-500 dark:text-zinc-400 hover:text-zinc-600 dark:hover:text-zinc-300 transition"
+            >
+              Load previous message
+            </button>
+          )}
+        </div>
+      )}
 
       {/* Messages */}
       <div className="flex flex-col-reverse mt-auto">
@@ -100,6 +118,7 @@ export const ChatMessages = ({
           </Fragment>
         ))}
       </div>
+      <div ref={bottomRef} />
     </div>
   );
 };
